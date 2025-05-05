d920c8e7be5f33b5cc57f6ddfaa2e933d6979783
Israel Carmona <jicmettesc@gmail.com>
2025-05-05 16:38:21 -0600

    Second commit

--- server.js
+++ server.js
@@ -1,25 +1,12 @@
 const express = require('express');
 const app = express();
-const router = express.Router();
  
-router.get('/home', (req, res) => {
-  res.send('Hello World, This is home router');
+app.get('/', (req, res) => {
+  res.send("Hello CSE 341!");
 });
  
-router.get('/profile', (req, res) => {
-  res.send('Hello World, This is profile router');
-});
- 
-router.get('/login', (req, res) => {
-  res.send('Hello World, This is login router');
-});
- 
-router.get('/logout', (req, res) => {
-  res.send('Hello World, This is logout router');
-});
- 
-app.use('/', router);
- 
-app.listen(process.env.PORT || 3000, () => {
+const port = 3000;
+
+app.listen(process.env.PORT || port, () => {
   console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
 });
